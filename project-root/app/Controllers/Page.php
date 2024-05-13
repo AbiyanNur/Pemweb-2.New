35d31422a4184911354f10ccac84d5588660bbc1
AbiyanNur <nurabiyan99@gmail.com>
2024-05-13 23:38:21 +0700

    Validation & Delete data

---
+++ project-root/app/Controllers/Page.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\BooksModel;
+
+class Page extends BaseController
+{
+    public function index()
+    {
+        $data = [
+            'title' => 'Home | Unipdu Press',
+            'tes' => ['satu', 'dua', 'tiga']
+        ];
+        return view('page/home', $data);
+    }
+
+    public function about()
+    {
+        $data = [
+            'title' => 'Home | Unipdu Press',
+            'tes' => ['satu', 'dua', 'tiga']
+        ];
+        return view('page/about', $data);
+    }
+
+    public function contact()
+    {
+        $data = [
+            'title' => 'Contact | Unipdu Press',
+            'alamat' => [
+                ['tipe' => 'Rumah', 'alamat' => 'Mojojejer', 'kota' => 'Jombang'],
+                ['tipe' => 'Kantor', 'alamat' => 'Mojowarno', 'kota' => 'Jombang']
+            ]
+        ];
+        return view('page/contact', $data);
+    }
+}
--- project-root/app/Views/pages/about.php
+++
@@ -1,8 +0,0 @@
-<?= $this->extend('layout/template'); ?>
-<?= $this->section('content'); ?>
-
-<body>
-    <h1>halaman About</h1>
-
-</body>
-<?= $this->endSection(); ?>
